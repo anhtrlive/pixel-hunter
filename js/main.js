c8f4dc6b7c3ff453e5e9ce0f4411803471670ec0
anhtrlive <artyukhovmaxim@fastmail.com>
2020-08-21 20:23:08 +0300

    add build

--- js/main.js
+++
@@ -1,3 +0,0 @@
-import Application from './router';
-
-Application.start();
--- js/utils/showScreen.js
+++
@@ -1,9 +0,0 @@
-const showScreen = (htmlElements) => {
-  const mainBlock = document.querySelector(`#main`);
-  if (mainBlock.lastElementChild !== null) {
-    [...mainBlock.children].forEach((element) => element.remove());
-  }
-  mainBlock.append(htmlElements);
-};
-
-export {showScreen};
